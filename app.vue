fe208d929fad64ae1b1ab77cddec46fdd18d09f2
jbalidiong <88189393+jbalidiong@users.noreply.github.com>
2024-05-31 00:34:32 +0800

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { DocumentData } from "firebase/firestore";
+
+const doc:DocumentData = {};
+
+</script>
+
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
